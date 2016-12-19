858dd108ee35104c64c74fe947f23b3924f7b143
Markus Herpich <markus@endocode.com>
2016-12-19 12:26:58 +0100

    Add test for unique keys in configuration

---
+++ tests/test_configuration.py
@@ -0,0 +1,16 @@
+import unittest
+from ausroller import read_variables
+from tempfile import NamedTemporaryFile
+
+class ConfigurationTest(unittest.TestCase):
+    def test_duplicate_config_key(self):
+        config = '{"test": "test1", "test":"test2"}'
+        with NamedTemporaryFile() as tempfile:
+            tempfile.write(config)
+            tempfile.flush()
+            with self.assertRaises(KeyError):
+                read_variables(tempfile.name)
+
+
+if __name__ == '__main__':
+    unittest.main()
