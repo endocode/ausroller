e0758f1d0c93a5d6a4f28112605df7e8beaf3164
Matthias Schmitz <matthias@endocode.com>
2017-01-23 17:12:39 +0100

    Make pip installable
    Add setup.py and move main code in ausroller package.
    Now ausroller can be installed via pip install which produces an
    executable ausroller. Keep still a minimal ausroller.py for backwards
    compatibility.
    
    close #15

--- ausroller.py
+++ ausroller.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python2
-# encoding: utf-8
+# encoding utf-8
+from ausroller import main
 
-from ausroller import Configuration
-from ausroller import Ausroller
-import logging
-
-
-def main():
-    # parse arguments from command line and
-    # read configuration file
-
-    c = Configuration()
-    c.parse_args()
-    logging.basicConfig(format='%(levelname)s:\t%(message)s',
-                        level=c.log_level)
-    c.read_config()
-
-    a = Ausroller(c)
-    resources = a.prepare_rollout()
-    a.rollout(resources)
-
-if __name__ == '__main__':
-    main()
+main()
--- ausroller/__init__.py
+++ ausroller/__init__.py
@@ -1,2 +1,3 @@
 from config import Configuration
 from core import Ausroller
+from main import main
