14d1b43dcc123c2d629abceaccf4b6584d3956b2
Julian Strobl <julian@endocode.com>
2017-02-03 14:52:35 +0100

    Bump to version v0.3.0-alpha

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ausroller',
-    version='0.2.0',
+    version='0.3.0-alpha',
     description='Deploy applications to kubernetes clusters',
     long_description=long_description,
 
