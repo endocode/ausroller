ed84ae53cc03a88e955e94de4b9da690d65c3bac
Markus Herpich <markus@endocode.com>
2017-02-03 08:57:25 +0100

    Add kubectl version check
    As we have introduced a dependency on kubernetes 1.4.0 for the
    get-contexts subcommand we need to check the version of the used kubectl
    client.
    Also check and warn if client version is older than the server's
    version.

--- ausroller/kube.py
+++ ausroller/kube.py
@@ -6,6 +6,7 @@ import sys
 
 kubectl_format = "{kubectl} --context={context} --namespace={namespace} {subcommand}"
 kubectl_default_bin = "kubectl"
+min_version = (1, 4, 0)
 
 
 class KubeCtlException(Exception):
@@ -34,6 +35,7 @@ class KubeCtl(object):
 
         if not skip_verify:
             try:
+                self.verify_version()
                 self.verify_context_available()
             except KubeCtlException as e:
                 logging.error("Configuring kubectl failed. [{}]".format(e))
@@ -71,3 +73,31 @@ class KubeCtl(object):
         if not match:
             raise KubeCtlException(
                 "The requested kubectl context [{}] is not available.".format(self.context))
+
+    def verify_version(self):
+        subcmd = "version --short"
+        versions = self._run(subcmd)
+        version_pattern = re.compile(
+            "Client Version: v(\d+)\.(\d+)\.(\d+)\nServer Version: v(\d+)\.(\d+)\.(\d+)")
+        match = version_pattern.match(versions)
+        if not match:
+            raise KubeCtlException(
+                "Failed to get kubectl versions. The api server might not be available.")
+        else:
+            client_version = (int(match.group(1)), int(
+                match.group(2)), int(match.group(3)))
+            server_version = (int(match.group(4)), int(
+                match.group(5)), int(match.group(6)))
+
+            logging.debug("Found versions: client[{}] server[{}]".format(
+                '.'.join(map(str, client_version)),
+                '.'.join(map(str, server_version))
+            ))
+            if client_version < min_version:
+                logging.error("kubectl client version is too old. Please upgrade to at least [{}.{}.{}]".format(
+                    *list(min_version)))
+                raise KubeCtlException("kubectl client version is too old.")
+
+            if client_version < server_version:
+                logging.warn(
+                    "kubectl client version is older than the server's version. Please consider upgrading your client.")
